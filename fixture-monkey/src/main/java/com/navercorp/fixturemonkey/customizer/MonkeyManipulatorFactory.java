9690ab0bbd77fbb3c5358dc6725e0b6f17fc4305
SooKim1110 <ohjskim@naver.com>
2023-03-19 00:20:54 +0900

    refactor

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/customizer/MonkeyManipulatorFactory.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/customizer/MonkeyManipulatorFactory.java
@@ -32,7 +32,7 @@ import org.apiguardian.api.API.Status;
 
 import net.jqwik.api.Arbitrary;
 
-import com.navercorp.fixturemonkey.api.customizer.MethodInterface;
+import com.navercorp.fixturemonkey.api.customizer.MethodInvocation;
 import com.navercorp.fixturemonkey.api.generator.ArbitraryContainerInfo;
 import com.navercorp.fixturemonkey.api.lazy.LazyArbitrary;
 import com.navercorp.fixturemonkey.customizer.InnerSpecState.ManipulatorHolderSet;
@@ -199,7 +199,7 @@ public final class MonkeyManipulatorFactory {
 			return new NodeNullityManipulator(false);
 		} else if (value instanceof Just) {
 			return new NodeSetJustManipulator((Just)value);
-		} else if (value instanceof MethodInterface) {
+		} else if (value instanceof MethodInvocation) {
 			return new NodeSetJustManipulator(Values.just(value));
 		} else if (value instanceof Arbitrary) {
 			return new NodeSetLazyManipulator<>(
